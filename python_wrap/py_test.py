ee74fe071a901e5253056316c30b5659db006bdd
Laura Hayes <hayesla@tcd.ie>
2016-12-08 20:22:29 +0000

    wrapper to run

---
+++ python_wrap/py_test.py
@@ -0,0 +1,39 @@
+import numpy as np
+import subprocess
+import os
+
+
+mins = 0
+while mins < 21:
+#parameters to do into lwpc inp file
+	workdir='/home/laura/lwpc_test/lwpc/python_wrap'
+	i_d = 'test_file'
+	freq = '24.000'
+	tx_lat = '44.633'
+	tx_lon = '67.283'
+	rx_lat = '53.091'
+	rx_lon = '-7.913'
+	power = '1.000e+03'
+	date = 'Jul/23/2016'
+	hhmm = '%02d:%02d' %(mins/60, mins%60)
+	
+	#creating inp file for the lwpc code
+	f = open('test_file.inp', 'w')
+	f.write('file-mds '+ workdir +'\n')
+	f.write('file-lwf '+ workdir +'\n')
+	f.write('tx '+ i_d +'\n')
+	f.write('tx-data '+ i_d + ' ' + freq + ' ' + tx_lat + ' ' + tx_lon +' '+power + ' 0 0 0'+'\n')
+	f.write('ionosphere lwpm ' + date + ' '+ hhmm + '\n')
+	f.write('receivers ' + rx_lat +' '+ rx_lon + '\n')
+	f.write('print-lwf 1 \n')
+	f.write('start \n')
+	f.write('quit \n')
+	f.close()
+	
+	#will run LWPC given the filename
+	input_file = 'test_file'
+	run_command = './test ' + input_file
+	run_lwpc = subprocess.call(run_command, shell = True)
+	print hhmm
+	mins = mins+10
+#os.remove('test_file.inp')
---
+++ python_wrap/reading_log.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import seaborn as sns
+
+#def main(argv):
+def read_lwpc_log(file_name):
+    
+    #file_name = argv[0]
+    a = open(file_name)
+    lines = a.readlines()
+    all_data = []
+    for i in range(len(lines)):
+        all_data.append(lines[i].split())
+
+    for i in range(len(all_data)):
+        try:
+            if all_data[i][0] == 'dist':
+                start_data = i
+            elif all_data[i][0] == 'nc':
+                end_data = i
+        except:
+            print('nada')
+
+
+    meta = all_data[0:start_data]
+    test = all_data[start_data+1: end_data]
+    rest = all_data[end_data:]
+
+    #takes care of the invalid literal for amp and phase sticking together
+    for i in range(len(test)):
+        if len(test[i]) == 8:
+            ll = test[i][-1].split('-')
+            test[i][-1] = ll[0]
+            test[i].append('-'+ll[1])
+      
+
+    #test = []
+    #for i in range(len(data)):
+    #    test.append(data[i].split())
+
+    test = np.array(test)
+    dist1 = []
+    dist2 = []
+    dist3 = []
+    for i in range(1, len(test)):
+        dist1.append(test[i][0])
+        dist2.append(test[i][3])
+        try:
+            dist3.append(test[i][6])
+        except:
+            print('end')
+    distance = dist1 + dist2 + dist3
+    for i in range(len(distance)):
+        distance[i] = float(distance[i])
+
+
+    amp1 = []
+    amp2 = []
+    amp3 = []
+    for i in range(1, len(test)):
+        amp1.append(test[i][1])
+        amp2.append(test[i][4])
+        try:
+            amp3.append(test[i][7])
+        except:
+            print('end')
+    amp = amp1 + amp2 + amp3
+    for i in range(len(amp)):
+        amp[i] = float(amp[i])
+
+    pha1 = []
+    pha2 = []
+    pha3 = []
+    for i in range(1, len(test)):
+        pha1.append(test[i][2])
+        pha2.append(test[i][5])
+        try:
+            pha3.append(test[i][8])
+        except:
+            print('end')
+    pha = pha1 + pha2 + pha3
+    for i in range(len(pha)):
+        pha[i] = float(pha[i])
+    return pha, amp, distance
+    '''
+    fig, ax = plt.subplots(2, sharex = True)
+    if file_name == 'gqd.log':
+	ax[0].set_ylim(0,500)
+	#ax[1].set_ylim(50, 110)
+        ax[0].set_xlim(0, 2000)
+    if file_name == 'naaa.log':
+        ax[0].set_xlim(0, 6550)
+	ax[0].set_ylim(-100,400)
+	ax[1].set_ylim(50, 120)
+    ax[0].plot(distance[0:-1], pha[0:-1], label = 'Phase')
+    ax[0].legend()
+    ax[0].set_xlabel('Distance (km)')
+    ax[1].plot(distance[0:-1], amp[0:-1], label = 'Amplitude', color = 'r')
+    ax[1].legend()
+    ax[1].set_xlabel('Distance (km)')
+    plt.tight_layout()
+   # plt.savefig('test1.png')
+    plt.show()'''
+    
+
+#if __name__ == "__main__":
+#    main(sys.argv[1:])
