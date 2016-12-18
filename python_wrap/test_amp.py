d35e11a82c88e7905fba4e0294952ed4bc617695
Laura Hayes <hayesla@tcd.ie>
2016-12-18 22:16:26 +0000

    comparing model

--- python_wrap/test_amp.py
+++ python_wrap/test_amp.py
@@ -6,7 +6,7 @@ from reading_msk_files import read_files
 from pandas import Series
 from sunpy.time import parse_time
 ###lwpc data##
-
+'''
 res = np.loadtxt('results_23_jul_h_76.dat', dtype = 'str')
 date = 'Jul/23/2016'
 res = np.array(res)
@@ -19,7 +19,7 @@ amp_lwpc = res[:,2]
 lwpc_amp = Series(amp_lwpc, index = tim)
 lwpc_pha = Series(pha_lwpc, index = tim)
 
-
+'''
 ###msk data ##
 
 name, msk_time, amp, pha = read_files('NAA20160723.txt')
@@ -28,4 +28,24 @@ msk_amp = Series(amp, index = msk_time)+107
 msk_pha = Series(pha, index = msk_time)
 
 
+res = np.loadtxt('h_and_beta.dat')
+h_prime = res[:,0]
+beta = res[:,1]
+phase = res[:,2]
+amp = res[:,3]
+dist = res[:,4]
 
+
+b_values = np.arange(0.3, 0.5, 0.01)
+h_values = np.arange(60, 80, 1)
+
+test = np.arange(0, 400, 20)
+new = []
+for i in range(len(test)):
+	if test[i]!= 380:
+		new.append(amp[test[i]:test[i+1]])
+	else:
+		new.append(amp[test[i]:])
+
+
+plt.contourf(b_values, h_values, new, cmap = 'magma')
