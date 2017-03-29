39e01089592aa5337557518dfa63627212a5de8b
hayesla <hayesla@tcd.ie>
2017-03-29 18:00:26 +0100

    update

--- python_wrap/n_e_modelling/testing_lwpc.py
+++ python_wrap/n_e_modelling/testing_lwpc.py
@@ -9,11 +9,11 @@ from scipy import ndimage
 smooth = ndimage.filters.uniform_filter
 from sunpy import lightcurve as lc
 import matplotlib.dates as dates
-
+import pandas as pd
 import seaborn as sns
 sns.set_style('ticks',{'xtick.direction':'in','ytick.direction':'in'})
 sns.set_context('paper', font_scale=1.8)
-
+from scipy.optimize import curve_fit
 
 ###lwpc data##
 res = np.loadtxt('24_lwpm_test.dat', dtype = 'str')
@@ -38,6 +38,12 @@ msk_pha_real = Series(pha_naa, index = msk_time)+45
 
 msk_amp_flare = msk_amp_real.truncate('2016-07-24 11:30','2016-07-24 16:00')
 msk_pha_flare = msk_pha_real.truncate('2016-07-24 11:30','2016-07-24 16:00')
+
+
+peak_t = np.loadtxt('gl_peaks.dat', dtype = 'str')
+peak_times = []
+for i in range(len(peak_t)):
+	peak_times.append(peak_t[i][0] + ' ' +peak_t[i][1])
 
 
 #goes setup
@@ -116,7 +122,7 @@ pha_l = np.array(pha_l)
 phase_test = msk_pha_flare.resample('1min', how = 'mean')
 amp_test = msk_amp_flare.resample('1min', how = 'mean')
 amp_b = sidy.resample('1min', how = 'mean')
-
+gll = gl.resample('1min', how = 'mean')
 indexx_phase = []
 indexx_amp = [] 
 for i in range(len(amp_b)):
@@ -126,6 +132,19 @@ for i in range(len(amp_b)):
     ind_amp = np.where(yyy == np.min(yyy))[0][0]
     indexx_phase.append(ind)
     indexx_amp.append(ind_amp)
+
+##finding indices of gl peak
+
+datt = []
+for i in range(len(peak_times)):
+	datt.append(datetime.datetime.strptime(peak_times[i], '%Y-%m-%d %H:%M:%S.%f'))
+
+
+indd = []
+for i in range(len(datt)):       
+	b = (datt[i] - amp_b.index).total_seconds()                        
+	c = np.where(np.abs(b) == np.min(np.abs(b)))[0][0]
+	indd.append(c)
 
 
 
@@ -219,9 +238,128 @@ def plot_elec_den(cmapp = 'gnuplot'):
     fig.autofmt_xdate()
     plt.tight_layout()
 
+#############################
+#
+# Recombination Coeff
+#
+############################
+def recombo():
+    h_p = smooth(h_prime[indexx_amp],10)
+    bet = smooth(beta[indexx_amp],10)
+    h  = np.arange(65, 85,0.1)
+    elec = []
+    for i in range(len(h_p)):
+        test = n_e(h, h_p[i], bet[i])
+        elec.append(test)
+
+    elec = np.array(elec)
+    elec = elec.T
 
 
-#function to plot 3d map of e density of time and h
+    #file containing info of max peaks in GOES
+    a = pd.read_csv('peak_n_e.txt', delimiter = ' ')
+    
+    #max GOES flux at each peak
+    I_max = a['max_I']
+
+    #time between peaks
+    dt = 90.
+
+    #elec den at max GOES Peak for each height
+    h_elec_max = []
+    for i in range(len(elec)):
+        ff = elec[i][indd]
+        h_elec_max.append(ff)
+
+    alphas = []
+    for i in range(len(h_elec_max)):
+        alpha = 1./(2*dt*h_elec_max[i])
+        alphas.append(alpha)
+
+    alphas = np.array(alphas)
+    each_peak = []
+    for i in range(len(I_max)):
+        each_peak.append(alphas[:,i])
+
+    plotting_lines = False
+    if plotting_lines:
+	    for i in range(0,len(alphas),40):
+		a = make_df(np.array(I_max), np.log10(alphas[i]), name1 = 'Flux Max', name2 = 'Recombination Coeff')
+		sns.regplot(x = 'Flux Max', y = 'Recombination Coeff', data = a, scatter_kws={"s": 50}, label = str(h[i])+' km')
+		plt.legend()
+	    
+
+
+    a_test = (0.51e-6)*np.exp(-0.165*h)
+    for i in range(len(each_peak)):
+        plt.plot(h, each_peak[i], label = 'Peak no: '+ str(i))
+        plt.plot(h, a_test, ls = '--')
+        
+
+    def f(x, A, B): # this is your 'straight line' y=f(x)
+        return A*x + B
+    plot_sur = False
+    if plot_sur:
+
+
+	    AA = []#LISTS OF SLOPES
+	    BB = []#LIST OF INTERCEPTS
+	    for i in range(0, len(alphas)):
+		#plt.plot(I_max, np.log10(alphas[i]), ls = ' ', marker = '.')
+		A, B = curve_fit(f, I_max, np.log10(alphas[i]))[0]
+		
+		AA.append(A)
+		BB.append(B)
+
+	    #now fit AA, BB in terms of heights
+	    A_slope, A_int = curve_fit(f, h, AA)[0]
+	    B_slope, B_int = curve_fit(f, h, BB)[0]
+
+	    def emp_al(h, I_max):
+		return (A_slope*h + A_int)*I_max + (B_slope*h+ B_int)
+
+	    II = np.array(I_max)
+	    II.sort()
+	    test_plot = []
+	    for i in range(len(h)):
+		test_plot.append(emp_al(h[i], np.array(II)))
+
+	    plt.imshow(test_plot, aspect = 'auto', cmap = 'viridis', extent = [II[0], II[-1], h[-1], h[0]])
+	     
+
+
+
+def make_df(arr1, arr2, name1 = 'name1', name2 = 'name2'):
+	d = {name1 : pd.Series(arr1), name2 : pd.Series(arr2)}
+	df = pd.DataFrame(d)
+	return df
+
+def plot_corr_e():
+    h_p = smooth(h_prime[indexx_amp],10)
+    bet = smooth(beta[indexx_amp],10)
+    h  = np.arange(65, 85,0.1)
+    elec = []
+    for i in range(len(h_p)):
+        test = n_e(h, h_p[i], bet[i])
+        elec.append(test)
+
+    elec = np.array(elec)
+    elec = elec.T
+
+    
+    for i in range(0,200,40):
+        a = make_df(np.array(gll), np.log10(elec[i][0:-1]), name1 = 'amp', name2 = 'elec')
+        sns.regplot(x = 'amp', y = 'elec', data = a, scatter_kws={"s": 80}, label = str(h[i])+' km')
+        plt.legend()
+    
+    	
+
+
+######################################################
+#
+# function to plot 3d map of e density of time and h
+#
+#######################################################
 def plot_elec_den_paper(cmapp = 'seismic'):
     h_p = smooth(h_prime[indexx_amp],10)
     bet = smooth(beta[indexx_amp],10)
@@ -240,11 +378,11 @@ def plot_elec_den_paper(cmapp = 'seismic'):
     testy = np.array(amp_b)
     ax[0].plot(sidy.index.to_pydatetime(), sidy, sns.xkcd_rgb["pale red"], label = 'BIRR VLF')
     ax[0].plot(sidy.index, smooth(sidy, 120), color = 'k', lw = 2, label = 'Smoothed BIRR VLF')
-    ax[0].set_ylabel('Amplitude in dB')
+    ax[0].set_ylabel('Amplitude in dBV')
     ax[0].xaxis.set_major_locator(dates.MinuteLocator(interval =30))
     ax[0].xaxis.set_major_formatter(dates.DateFormatter('%H.%M'))
     ax[0].xaxis.grid(True, which="major")
-    ax[0].set_title('VLF Amplitude Pulsations')
+    ax[0].set_title('(a) VLF Amplitude Pulsations')
 
     ax1 = ax[1].imshow(np.log10(elec.T), aspect = 'auto', origin = 'lower', extent = [x_lims[0], x_lims[1], 65, 85], cmap = cmapp)
     ax[1].xaxis_date()
@@ -255,7 +393,7 @@ def plot_elec_den_paper(cmapp = 'seismic'):
     ax[1].xaxis.grid(True, which="major", color = 'grey')
     ax[1].set_ylabel('Altitude (km)')
     ax[1].set_xlabel('Start time 24-Jul-2016 11:00 UT')
-    ax[1].set_title('Electron Density in Lower Ionosphere')
+    ax[1].set_title('(b) Electron Density in Lower Ionosphere')
 
     cbaxes = fig.add_axes([0.85, 0.08, 0.02, 0.4]) 
     cb = plt.colorbar(ax1, cax = cbaxes)  
